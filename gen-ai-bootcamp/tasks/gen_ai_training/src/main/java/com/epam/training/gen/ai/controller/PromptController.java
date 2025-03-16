2355c98b4e0c6670a40c3c36e3bc624984aca5b5
Naresh_Addanki <naresh_addanki@epam.com>
2025-03-16 16:23:58 +0530

    Updated javadoc

--- gen-ai-bootcamp/tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/controller/PromptController.java
+++ gen-ai-bootcamp/tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/controller/PromptController.java
@@ -2,6 +2,8 @@ package com.epam.training.gen.ai.controller;
 
 import com.epam.training.gen.ai.promt.SimplePromptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +17,25 @@ public class PromptController {
     @Autowired
     SimplePromptService promptService;
 
+    /**
+     * Endpoint to generate a response based on the provided prompt.
+     * <p>
+     * The provided prompt is sent to the Azure OpenAI service via the
+     * {@link SimplePromptService} to generate a response. The response
+     * is returned as a string.
+     * </p>
+     *
+     * @param prompt the text input to be used as a prompt for generating a response
+     * @return the generated response as a string
+     */
     @GetMapping(value = "/api/generate-response")
-    public String generateResponse(@RequestParam(value = "promt") String promt) throws IOException, InterruptedException {
-       var chats = promptService.getChatCompletions(promt);
-        return chats.toString();
+    public ResponseEntity<String> generateResponse(@RequestParam(value = "prompt") String prompt) throws IOException, InterruptedException {
+        try {
+            var chats = promptService.getChatCompletions(prompt);
+            return ResponseEntity.ok(chats.toString());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error generating response");
+        }
 
     }
 
