09ed185412c828c60f9ef4df5b52acf6d05ce360
Naresh_Addanki <naresh_addanki@epam.com>
2025-03-21 16:27:13 +0530

    Corrected indentation.

--- gen-ai-bootcamp/tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/Constants.java
+++ gen-ai-bootcamp/tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/Constants.java
@@ -2,5 +2,5 @@ package com.epam.training.gen.ai;
 
 public class Constants {
     public static final double TEMPERATURE = 0.9;
-    public static final int MAX_TOKENS = 50;
+    public static final int MAX_TOKENS = 200;
 }
